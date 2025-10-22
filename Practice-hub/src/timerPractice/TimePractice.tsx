e7f40cf6b89d7b7ce9fb03c2277b706eb9bd0269
gilotin <toshevnikolay@gmail.com>
2025-10-23 00:25:38 +0300

    feat(time practice): finished with component logic

--- Practice-hub/src/timerPractice/TimePractice.tsx
+++ Practice-hub/src/timerPractice/TimePractice.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 export function TimePracticeDisplay() {
-    const [showCountdown, setShowCountdown] = useState(false);
-    const [lockButton, setLockButton] = useState(false);
-    const [delayedStart, setDelayedStart] = useState(Number);
+    const [showCountdown, setShowCountdown] = useState<boolean>(false);
+    const [lockButton, setLockButton] = useState<boolean>(false);
+    const [delayedStart, setDelayedStart] = useState<number>(0);
+    const [startTime, setStartTime] = useState<number>(0);
+    const [score, setScore] = useState<string>("0");
 
     const LOWER_INTEGER: number = 1;
 
@@ -19,26 +21,44 @@ export function TimePracticeDisplay() {
         setLockButton((prevState) => true);
         setTimeout(() => {
             setShowCountdown((prevState) => !prevState);
+            setStartTime((prevState) => generateTime());
         }, delayedStart);
     }
 
     function onClickResetBtn(e: React.MouseEvent<HTMLButtonElement>) {
-        setDelayedStart((prevState) => generateNumber());
-        setShowCountdown((prevState) => false);
-        setLockButton((prevState) => false);
+        setDelayedStart(generateNumber());
+        setShowCountdown(false);
+        setLockButton(false);
+        setStartTime(0);
+        setScore("0");
+    }
+
+    function handleSpeedClickButton() {
+        const clickTime = generateTime();
+        const result = (clickTime - startTime).toFixed();
+        setScore(result);
+    }
+
+    function generateTime() {
+        const generateTime = performance.now();
+        return generateTime;
     }
 
     return (
         <>
-            <div className="wrapper-time-practice ">
+            <div className="wrapper-time-practice">
                 <section className="score-board">
-                    <p>Best time: 234ms </p>
+                    <p>Best time: {score} ms </p>
                 </section>
-                <section className="game-section">
-                    <div className="display-section">
-                        <p className="glow-text">{showCountdown ? "Click!" : "press Start"}</p>
-                    </div>
-                </section>
+                {lockButton && showCountdown ? (
+                    <section onClick={handleSpeedClickButton} className="game-section">
+                        <p className="glow-text">Click!!!</p>
+                    </section>
+                ) : (
+                    <section className="game-section">
+                        <p className="glow-text">Press Start</p>
+                    </section>
+                )}
                 <section className="user-interface">
                     <button className="start-btn" disabled={lockButton} onClick={OnClickStartBtn}>
                         START
--- Practice-hub/vite.config.ts
+++ Practice-hub/vite.config.ts
@@ -5,4 +5,9 @@ import Checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), Checker({ typescript: true })],
+    server: {
+        host: true, // allows access from LAN (0.0.0.0)
+        port: 5173, // optional, default is 5173
+        strictPort: false, // true will fail if port is taken, false will pick next available
+    },
 });
