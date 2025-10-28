8c685b1e54df57a0e7e8c78b3233c9f452d5bc87
gilotin <toshevnikolay@gmail.com>
2025-10-28 13:15:54 +0200

    feat(pokemon-card): load name, sprite, height, and weight from API

--- Practice-hub/src/PokemonCards/PokemonCards.tsx
+++ Practice-hub/src/PokemonCards/PokemonCards.tsx
@@ -1,6 +1,79 @@
+import { useEffect, useState, type MouseEventHandler } from "react";
+
+type PokemonData = {
+    id: number;
+    name: string;
+    sprites: {
+        front_default: string;
+    };
+    height: number;
+    weight: number;
+    stats: [
+        {
+            base_stat: number;
+            stat: {
+                name: string;
+            };
+        }
+    ];
+    types: [
+        {
+            type: {
+                name: string;
+            };
+        }
+    ];
+};
+
 export function PokemonCard() {
+    const [pokemonData, setPokemonData] = useState<PokemonData | null>(null);
+
+    const url: string = `https://pokeapi.co/api/v2/pokemon/${"1"}`;
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        async function fetchData() {
+            const response = await fetch(url, { signal });
+            try {
+                if (!response.ok) {
+                    throw new Error(`Response status:${response.status}`);
+                }
+                const data = await response.json();
+                setPokemonData(data);
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    if (err.name === "AbortError") {
+                        console.log("Fetch aborted");
+                    }
+                } else {
+                    console.error(err);
+                }
+            }
+        }
+
+        fetchData();
+        return () => {
+            controller.abort();
+        };
+    }, [url]);
+
+    const pokemonStats = pokemonData?.stats.map((pokemonStats, statId) => {
+        return (
+            <li key={statId}>
+                {pokemonStats?.stat.name}:{pokemonStats?.base_stat}
+            </li>
+        );
+    });
+
+    const pokemonTypes = pokemonData?.types.map((pokemonType, typeId) => {
+        return <li key={typeId + 0.1}>{pokemonType?.type.name}</li>;
+    });
+
     return (
         <>
+            {console.log(pokemonData)}
             <div className="wrapper">
                 <h1>Pokemon Cards</h1>
                 <form method="POST">
@@ -11,23 +84,18 @@ export function PokemonCard() {
 
             <section className="pokemon-card">
                 <div className="wrapper">
-                    <h2 className="pokemon-card__header">Pikachu</h2>
-                    <img src="" alt="" />
+                    <h2 className="pokemon-card__header">{pokemonData?.name}</h2>
+                    <img
+                        src={pokemonData?.sprites.front_default}
+                        alt={`pokemon named${pokemonData?.name}`}
+                    />
                     <p className="pokemon-card__description">some description with flavour text</p>
-                    <ul className="pokemon-card__types">
-                        <li>grass</li>
-                        <li>poison</li>
-                    </ul>
+                    <ul className="pokemon-card__types">{pokemonTypes}</ul>
 
-                    <ul className="pokemon-card__stats">
-                        <li>HP:100</li>
-                        <li>Att:10</li>
-                        <li>Def:50</li>
-                        <li>Spd: 25</li>
-                    </ul>
+                    <ul className="pokemon-card__stats">{pokemonStats}</ul>
                     <ul className="pokemon-card__measures">
-                        <li>height: 20</li>
-                        <li>weight 10</li>
+                        <li>height: {pokemonData?.height}</li>
+                        <li>weight {pokemonData?.weight}</li>
                     </ul>
                     <ul className="pokemon-card__abilities">
                         <li>Attack1:</li>
