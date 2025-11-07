1cdfe9188ac60dc232382b1126cf6116bf8b6981
gilotin <toshevnikolay@gmail.com>
2025-11-07 15:07:08 +0200

    fix(pokemon-card): Add additional info to some errors

--- Practice-hub/src/features/PokemonCards/api/fetchPokemonData.ts
+++ Practice-hub/src/features/PokemonCards/api/fetchPokemonData.ts
@@ -5,7 +5,7 @@ export default async function fetchData<T>(url: string, signal: AbortSignal): Pr
     }
 
     if (!response.ok) {
-        throw new Error(`Fetch failed: ${response.status}`);
+        throw new Error(`Fetch failed: ${response.status}.Try again.`);
     }
     const data: T = await response.json();
     return data;
