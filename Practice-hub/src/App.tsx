e0caf11f4f1603ab5b7b7b04123bbc9a19e4dd40
gilotin <toshevnikolay@gmail.com>
2025-11-25 17:03:28 +0200

    feat: Add simple navigation to the project

--- Practice-hub/src/App.tsx
+++ Practice-hub/src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { PokemonCard } from "./features/PokemonCards/PokemonCards";
-// import { TimePracticeDisplay } from "./timerPractice/TimePractice";
+import { MoodTracker } from "./features/MoodTracker/MoodTracker";
+import { TimePracticeDisplay } from "./features/timerPractice/TimePractice";
+import { Navigation } from "./features/Navigation/Navigation";
 
 function App() {
     return (
         <>
-            <PokemonCard />
+            <BrowserRouter>
+                <Navigation />
+                <Routes>
+                    <Route path="/pokemon-card" element={<PokemonCard />} />
+                    <Route path="/mood-tracker" element={<MoodTracker />} />
+                    <Route path="/time-practice" element={<TimePracticeDisplay />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
---
+++ Practice-hub/src/features/Navigation/Navigation.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router";
+import styles from "./Navigation.module.css";
+
+export function Navigation() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul className={styles.nav}>
+                        <li>
+                            <NavLink to="/pokemon-card">Pokemon Cards</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/mood-tracker">Mood Tracker</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/time-practice">Time Practice</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
